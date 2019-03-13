fed6d9442a801410c76c445af2c9e8bea79e4606
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-13 16:46:51 +0000

    Deploy lampholder/feature-dashboard to github.com/lampholder/feature-dashboard.git:gh-pages

---
+++ build/precache-manifest.9b670ec8f34a0a0a4e814fba778d871d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7576adf5f829748012cb",
+    "url": "/feature-dashboard/static/js/runtime~main.7576adf5.js"
+  },
+  {
+    "revision": "ec80c3a6f99c9805e986",
+    "url": "/feature-dashboard/static/js/main.ec80c3a6.chunk.js"
+  },
+  {
+    "revision": "d0d57ce1ac37b2e3dfbf",
+    "url": "/feature-dashboard/static/js/2.d0d57ce1.chunk.js"
+  },
+  {
+    "revision": "ec80c3a6f99c9805e986",
+    "url": "/feature-dashboard/static/css/main.eba7a52c.chunk.css"
+  },
+  {
+    "revision": "456ce80875a6729ea5864806ffdb292e",
+    "url": "/feature-dashboard/index.html"
+  }
+];
