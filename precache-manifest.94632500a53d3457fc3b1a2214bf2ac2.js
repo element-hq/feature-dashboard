f331fb8743028f0753b6876f3ae1f5b41c9eb547
Tom Lant <lampholder@gmail.com>
2019-05-01 11:07:24 +0100

    Updates

---
+++ precache-manifest.94632500a53d3457fc3b1a2214bf2ac2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0ee1587b91df9fb76d4",
+    "url": "/feature-dashboard/static/css/main.33957c6c.chunk.css"
+  },
+  {
+    "revision": "a0ee1587b91df9fb76d4",
+    "url": "/feature-dashboard/static/js/main.a0ee1587.chunk.js"
+  },
+  {
+    "revision": "7576adf5f829748012cb",
+    "url": "/feature-dashboard/static/js/runtime~main.7576adf5.js"
+  },
+  {
+    "revision": "76ab1918a76b84cd0ec6",
+    "url": "/feature-dashboard/static/js/2.76ab1918.chunk.js"
+  },
+  {
+    "revision": "9e211907ff49682d245bc988227f125d",
+    "url": "/feature-dashboard/index.html"
+  }
+];
