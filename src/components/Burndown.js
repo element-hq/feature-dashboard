acc200777e8b77b4964fb7425d224be7cee7e622
J. Ryan Stinnett <jryans@gmail.com>
2019-07-01 15:32:22 +0100

    Prototype of phased burndown

--- src/components/Burndown.js
+++ src/components/Burndown.js
@@ -22,7 +22,9 @@ import { Line } from 'react-chartjs-2';
 class Burndown extends Component {
 
     render() {
-        if (this.props.issues.length === 0) {
+        let { issues } = this.props;
+
+        if (issues.length === 0) {
             return (
                 <div className="Burndown raised-box">
                     <h3>Loading data...</h3>
@@ -33,11 +35,10 @@ class Burndown extends Component {
         let dates = [];
         let issueCounts = {};
 
+        // Initialise dates array and issue count per day for all relevant dates
         let date = new Date(
             Math.min(
-                ...this.props.issues.map(
-                    issue => new Date(issue.githubIssue.created_at)
-                )
+                ...issues.map(issue => new Date(issue.githubIssue.created_at))
             )
         );
         let today = new Date();
@@ -45,68 +46,112 @@ class Burndown extends Component {
         while (date < tomorrow) {
             let day = dateFormat(date, 'yyyy-mm-dd');
             dates.push(day);
-            issueCounts[day] = 0;
+            issueCounts[day] = {};
             date.setDate(date.getDate() + 1);
         }
 
-        this.props.issues.forEach(issue => {
-            let start = dates.indexOf(dateFormat(issue.githubIssue.created_at, 'yyyy-mm-dd'));
-            let end = issue.githubIssue.closed_at ? dates.indexOf(dateFormat(issue.githubIssue.closed_at, 'yyyy-mm-dd')) : dates.length;
-            for (let n = start; n < end; n++) {
-                issueCounts[dates[n]] += 1;
+        // Attempt to bucket issues by phase
+        // TODO: Extract this out as a generic issue categoriser
+        let label = issue => {
+            let phases = issue.labels.filter(label => label.name.startsWith('phase:'));
+            if (phases.length > 0) {
+                return phases[0].name;
             }
+            return null;
+        };
+        let sort = (a, b) => {
+            return Number(a.split(":")[1]) - Number(b.split(":")[1]);
+        };
+        // TODO: Do we wanted unbucketed for this view?
+        // let unbucketed = 'unphased';
+        let unbucketed;
+
+        let headings = [...new Set(issues.filter(label).map(label))].sort(sort);
+        let buckets = {};
+        headings.forEach(heading => {
+            buckets[heading] = issues.filter(item => label(item) === heading);
         });
 
-        let datasets = [
-            {
-                label: 'Open issues',
-                data: dates.map(date => issueCounts[date]),
-                lineTension: 0,
-            }
-        ];
+        // If we're interested in issues that weren't matched by the filter,
+        // throw them into an 'unbucketed' category.
+        if (unbucketed) {
+            let unbucketedItems = issues.filter(item =>
+                !Object.values(buckets).reduce(Array.concat, []).includes(item));
 
-        let maxDate = Object.keys(issueCounts)
-            .reduce((a, b) => {
-                if (issueCounts[a] === issueCounts[b]) {
-                    return a < b ? a : b;
-                }
-                else {
-                    return issueCounts[a] > issueCounts[b] ? a : b
-                }
-            });
-
-        let maxIssues = issueCounts[maxDate];
-        let todaysIssues = issueCounts[dates[dates.length - 1]];
-        let elapsedDays = (dates.length - dates.indexOf(maxDate) - 1);
-        let rate = (maxIssues - todaysIssues) / elapsedDays;
-        let totalDays = dates.indexOf(maxDate) + 1 + (maxIssues / rate);
-        let remainingDays = totalDays - dates.length;
-
-        if (remainingDays !== Infinity) {
-            let date = new Date(dates[dates.length - 1]);
-            for (let i = 0; i < remainingDays + 1; i++) {
-                date.setDate(date.getDate() + 1);
-                dates.push(dateFormat(date, 'yyyy-mm-dd'));
+            if (unbucketedItems.length > 0) {
+                buckets[unbucketed] = unbucketedItems;
             }
-            let projection = [];
-            for (let i = 0; i < dates.indexOf(maxDate); i++) {
-                projection.push(null);
-            }
-            for (let i = 0; i < elapsedDays + remainingDays; i++) {
-                projection.push(maxIssues - (i * rate));
-            }
-            projection.push(0);
-            datasets.push({
-                label: 'Projected delivery',
-                data: projection,
-                lineTension: 0,
-                fill: false,
-                pointRadius: 0,
-                borderColor: '#738d04',
-                borderWidth: 1
-            });
-
         }
+
+        let datasets = [];
+
+        // Create a dataset for each bucket
+        Object.keys(buckets).forEach(bucket => {
+            // Initialise counts to 0 for this bucket for all dates
+            Object.keys(issueCounts).forEach(date => {
+                issueCounts[date][bucket] = 0;
+            });
+
+            buckets[bucket].forEach(issue => {
+                let start = dates.indexOf(dateFormat(issue.githubIssue.created_at, 'yyyy-mm-dd'));
+                let end = issue.githubIssue.closed_at ? dates.indexOf(dateFormat(issue.githubIssue.closed_at, 'yyyy-mm-dd')) : dates.length;
+                for (let n = start; n < end; n++) {
+                    issueCounts[dates[n]][bucket] += 1;
+                }
+            });
+            datasets.push({
+                label: `Open ${bucket} issues`,
+                data: dates.map(date => issueCounts[date][bucket]),
+                lineTension: 0,
+            });
+        });
+
+        let todaysDate = dates[dates.length - 1];
+
+        // Attempt to project a delivery date for each bucket
+        Object.keys(buckets).forEach(bucket => {
+            let maxDate = Object.keys(issueCounts)
+                .reduce((a, b) => {
+                    if (issueCounts[a][bucket] === issueCounts[b][bucket]) {
+                        return a < b ? a : b;
+                    }
+                    return issueCounts[a][bucket] > issueCounts[b][bucket] ? a : b;
+                });
+            let maxIssues = issueCounts[maxDate][bucket];
+            let todaysIssues = issueCounts[todaysDate][bucket];
+            let fromMaxToTodayDays = (dates.length - dates.indexOf(maxDate) - 1);
+            // TODO: Use a better estimate of team velocity
+            let rate = (maxIssues - todaysIssues) / fromMaxToTodayDays;
+            let totalDays = dates.indexOf(maxDate) + 1 + (maxIssues / rate);
+            let remainingDays = totalDays - dates.length;
+
+            if (todaysIssues > 0 && remainingDays !== Infinity) {
+                let date = new Date(todaysDate);
+                for (let i = 0; i < remainingDays + 1; i++) {
+                    let day = dateFormat(date, 'yyyy-mm-dd');
+                    dates.push(day);
+                    issueCounts[day] = {};
+                    date.setDate(date.getDate() + 1);
+                }
+                let projection = [];
+                for (let i = 0; i < dates.indexOf(maxDate) + fromMaxToTodayDays + 1; i++) {
+                    projection.push(null);
+                }
+                for (let i = fromMaxToTodayDays; i < totalDays; i++) {
+                    projection.push(maxIssues - (i * rate));
+                }
+                projection.push(0);
+                datasets.push({
+                    label: `Projected ${bucket} delivery`,
+                    data: projection,
+                    lineTension: 0,
+                    fill: false,
+                    pointRadius: 0,
+                    borderColor: '#738d04',
+                    borderWidth: 1,
+                });
+            }
+        });
 
         let data = {
             labels: dates,
@@ -115,6 +160,7 @@ class Burndown extends Component {
         let options = {
             scales: {
                 yAxes: [{
+                    stacked: true,
                     ticks: {
                         min: 0
                     }
